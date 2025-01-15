9c8995eb4fdf5508120c8e0b59c7db2559af5de6
cuongnv <cnv>
2025-01-15 13:37:31 +0900

    update

--- .ctupe/threads/ImgOffline.lua
+++ .ctupe/threads/ImgOffline.lua
@@ -10,16 +10,21 @@ local uChn = Thread.GetOfflineUrlChannel()
 while true do
     local uObj = uChn:pop()
     if uObj then
+        local url = uObj.url
         local width = uObj.width
         local height = uObj.height
 
-        local _,_ = pcall(function()
-            local pathImg = uObj.basePath .. "/" .. uObj.id .. "/" .. Config.SAVE_THUMBNAIL_PATH
-            local file = io.open(pathImg, "rb")
-            if file then
-                local data = file:read("*all")
-                file:close()
-                local file_data = love.filesystem.newFileData(data, '' ,"file")
+        local buffer = {}
+        local requestSuccess,_ = pcall(function()
+            http.request{
+                url = url,
+                sink = ltn12.sink.table(buffer)
+            }
+        end)
+
+        if requestSuccess then
+            local _,_ = pcall(function()
+                local file_data = love.filesystem.newFileData(table.concat(buffer), '' ,'file')
                 rsChn:push(
                 {
                     id = uObj.id,
@@ -28,9 +33,29 @@ while true do
                     height = height,
                     type = uObj.type
                 })
-            else
-                print("File not found: " .. pathImg)
-            end
-        end)
+            end)
+        end
+        -- local width = uObj.width
+        -- local height = uObj.height
+
+        -- local _,_ = pcall(function()
+        --     local pathImg = uObj.basePath .. "/" .. uObj.id .. "/" .. Config.SAVE_THUMBNAIL_PATH
+        --     local file = io.open(pathImg, "rb")
+        --     if file then
+        --         local data = file:read("*all")
+        --         file:close()
+        --         local file_data = love.filesystem.newFileData(data, '' ,"file")
+        --         rsChn:push(
+        --         {
+        --             id = uObj.id,
+        --             imgData = file_data,
+        --             width = width,
+        --             height = height,
+        --             type = uObj.type
+        --         })
+        --     else
+        --         print("File not found: " .. pathImg)
+        --     end
+        -- end)
     end
 end
