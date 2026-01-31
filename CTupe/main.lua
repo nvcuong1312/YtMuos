35ca812660eb1fccb133105aca07e7f93c75a118
cuongnv <nvcuong1312@gmail.com>
2026-01-31 16:27:40 +0700

    v1.2.1

--- CTupe/main.lua
+++ CTupe/main.lua
@@ -10,7 +10,7 @@ local Text = require("text")
 local Color = require("color")
 local Icon = require("icon")
 
-local version = "v1.2 - Glad you like it!!! - by nvcuong1312"
+local version = "v1.2.1 - Glad you like it!!! - by nvcuong1312"
 local hasAPIKEY = false
 
 local isShowOnlineList = true
